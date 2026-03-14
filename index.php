ce49d4b6aabbc42f681e792c03b3fc89f50acfc2
noobCoder2 <arjaybastillador29@gmail.com>
2026-03-15 06:03:42 +0800

    apply a bootstrap css

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Management System</title>
+    <!-- Bootstrap Css -->
+    <linkhref="bootstrap-5.3.8-dist/bootstrap-5.3.8-dist/css/bootstrap.min.css"rel="stylesheet"/>
+    <linkhref="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css"rel="stylesheet"/>
+    <!-- Custom Css -->
+    <link href="./css/global.css" rel="stylesheet">
 </head>
 <body>
+    <div class="container"></div>
     <h1>Hello World</h1>
     <h2>Student Management System</h2>
     <h3>Hello Po</h3>
