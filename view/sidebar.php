f0f07c48fd98377bd60717f2c5ab0c8db7d78764
noobCoder2 <arjaybastillador29@gmail.com>
2026-03-15 09:09:06 +0800

    hirap sa sidebar

--- view/sidebar.php
+++ view/sidebar.php
@@ -5,8 +5,40 @@
         </div>
 
         <div class="text header-text">
-            <span class="name">Student</span>
+            <span class="name">School MS</span>
             <span class="name2">Management System</span>
         </div>
     </header>
+
+    <ul class="nav-links">
+        <li>
+            <a href="#">
+                <i class="bi bi-speedometer2"></i>
+                <span class="link-name">Dashboard</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="bi bi-people"></i>
+                <span class="link-name">Students</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="bi bi-journal-text"></i>
+                <span class="link-name">Courses</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="bi bi-calendar-check"></i>
+                <span class="link-name">Attendance</span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <i class="bi bi-bar-chart-line"></i>
+                <span class="link-name">Grades</span>
+            </a>
+        </li>
 </div>
