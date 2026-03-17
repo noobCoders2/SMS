2825645ed8af1d53c1f5517c3db5403033370537
noobCoder2 <arjaybastillador29@gmail.com>
2026-03-18 05:46:30 +0800

    Updade 2.0

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +13,7 @@
     <link href="./css/global.css" rel="stylesheet">
     <link href="./css/sidebar.css" rel="stylesheet">
 </head>
+
 <body>
     <!-- Sidebar -->
     <?php include('./view/sidebar.php'); ?>
@@ -25,14 +27,13 @@
 
 
         <!-- foooter -->
-    <?php include('./view/footer.php'); ?>
+        <?php include('./view/footer.php'); ?>
     </section>
-
 
     <!-- Bootstrap JS  -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Custom JS -->
-<script src="./script/sidebar.js"></script>
+    <script src="./script/sidebar.js"></script>
     <script src="./script/footer_year.js"></script>
 
 </html>
--- view/sidebar.php
+++ view/sidebar.php
@@ -1,83 +1,92 @@
 <div class="sidebar">
+    <!-- Header of Sidebar -->
     <header class="logo-details d-flex align-items-center">
         <div class="logo-text">
             <span class="logo-name">SMS</span>
         </div>
 
-        <div class="text header-text">
+        <div class="header-text">
             <span class="name">School MS</span>
             <span class="name2">Management System</span>
         </div>
     </header>
 
+    <!-- Content of Sidebar -->
     <ul class="nav-links">
-        <li>
-            <a href="#">
-                <i class="bi bi-speedometer2"></i>
-                <span class="link-name">Dashboard</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-people"></i>
-                <span class="link-name">Student Management</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-journal-text"></i>
-                <span class="link-name">Employee Management</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-calendar-check"></i>
-                <span class="link-name">Academic Management</span>
+            <li>
+                <a href="#">
+                    <i class="bi bi-speedometer2"></i>
+                    <span class="link-name">Dashboard</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-people"></i>
+                    <span class="link-name">Student Management</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-journal-text"></i>
+                    <span class="link-name">Employee Management</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-calendar-check"></i>
+                    <span class="link-name">Academic Management</span>
+                </a>
                 <div class="dropdown">
-                    <p>Courses</p>
-                    <p>Subjects</p>
-                    <p>Class Schedule</p>
+                    <p><i class="bi bi-book"></i> Courses</p>
+                    <p><i class="bi bi-easel"></i> Subjects</p>
+                    <p><i class="bi bi-calendar3"></i> Class Schedule</p>
                 </div>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-bar-chart-line"></i>
-                <span class="link-name">Enrollment</span>
-            </a>
-        </li>
-        <li>
-            <a href="">
-                <span>Grade Management</span>
-            </a>
-        </li>
-        <li>
-            <a href="">
-                <span>Attendance</span>
-            </a>
-        </li>
-        <li>
-            <a href="">
-                <span>Accounting</span>
-            </a>
-            <div class="dropdown">
-                <p>Payment</p>
-                <p>Fee Structure</p>
-            </div>
-        </li>
-        <li>
-            <a href="">
-                <span>Payroll</span>
-            </a>
-        </li>
-        <li>
-            <a href="">
-                <span>Reports</span>
-            </a>
-        </li>
-        <li>
-            <a href="">
-                <span>System Settings</span>
-            </a>
-        </li>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-person-plus"></i>
+                    <span class="link-name">Enrollment</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-award"></i>
+                    <span class="link-name">Grade Management</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-check-circle"></i>
+                    <span class="link-name">Attendance</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-currency-dollar"></i>
+                    <span class="link-name">Accounting</span>
+                </a>
+                <div class="dropdown">
+                    <p><i class="bi bi-credit-card"></i> Payment</p>
+                    <p><i class="bi bi-receipt"></i> Fee Structure</p>
+                </div>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-cash-stack"></i>
+                    <span class="link-name">Payroll</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-file-earmark-bar-graph"></i>
+                    <span class="link-name">Reports</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-gear"></i>
+                    <span class="link-name">System Settings</span>
+                </a>
+            </li>
+        </ul>
 </div>
