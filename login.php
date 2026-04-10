4cfe575976e1c3a57b8ed86a95652b4bb5c25199
noobCoder2 <arjaybastillador29@gmail.com>
2026-04-11 06:15:49 +0800

    update add login php, ---next todo database

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+<body>
+
+    <div class="main">
+
+        <div class="left">
+
+            <h1>Welcome Back</h1>
+            <p>I'm glad your back to Ericson Academy College </p>
+        </div>
+
+        <div class="card-login">
+            <input type="text" placeholder="Email or phone">
+            <input type="password" placeholder="Password">
+
+            <button class="login-btn">Log In</button>
+
+            <div class="forgot">
+                <a href="#">Forgot password?</a>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
--- readme.md
+++ readme.md
@@ -1,12 +1,15 @@
 # Project Title: School Management System
 
-# Problem: Standby
+# Problem: 
+```
+No login test
+
+```
 
 # Info
 ```
 In view/partials all reusable component like sidebar etc...
 In Controller folder all request comes from view
-
 
 ```
 
