f156efa3fcc40e4269407db9161eb6bece36279a
noobCoder2 <arjaybastillador29@gmail.com>
2026-04-19 07:07:36 +0800

    update adding a document or DOMContentLoaded

--- assets/js/mainStudent.js
+++ assets/js/mainStudent.js
@@ -4,9 +4,11 @@ import { initSearch } from './student/searchStudent.js';
 import { initFilterStudent } from './student/filterStudent.js';
 import { initStudentModals } from './student/modalStudent.js';
 
+document.addEventListener('DOMContentLoaded', () => {
 // Initialize all export functions
 initStudentModals();
 initFilterStudent();
 initExport();
 initFormValidation();
 initSearch();
+});
